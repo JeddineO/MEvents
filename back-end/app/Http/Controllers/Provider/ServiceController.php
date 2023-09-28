29c89ce485a990086de6f40b4f72ac8c2ef95779
Ismail ZAHIR <ismailza407@gmail.com>
2023-09-28 11:19:27 +0100

    Provider services

---
+++ back-end/app/Http/Controllers/Provider/ServiceController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Provider;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ServiceRequest;
+use App\Models\Service;
+
+class ServiceController extends Controller
+{
+    
+    public function index()
+    {
+        
+    }
+
+    public function store(ServiceRequest $request)
+    {
+        
+    }
+
+    public function edit(Service $service)
+    {
+        
+    }
+
+    public function update(ServiceRequest $request, Service $service)
+    {
+        
+    }
+
+    public function destroy(Service $service)
+    {
+        
+    }
+
+}
